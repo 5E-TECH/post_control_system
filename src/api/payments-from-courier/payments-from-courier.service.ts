d763268eca3bfd16ec48170b06cf0c5164c84b32
Urozov04 <durozov46@gmail.com>
2025-06-28 15:31:20 +0500

    Added new branch(dilshodbek)

--- src/api/payments-from-courier/payments-from-courier.service.ts
+++ src/api/payments-from-courier/payments-from-courier.service.ts
@@ -1,10 +1,11 @@
 import { PaymentsFromCourierEntity } from './../../core/entity/payments.from.courier';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePaymentsFromCourierDto } from './dto/create-payments-from-courier.dto';
 import { UpdatePaymentsFromCourierDto } from './dto/update-payments-from-courier.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { catchError } from 'src/infrastructure/lib/response';
+import { PaymentMethod } from 'src/common/enums';
 
 @Injectable()
 export class PaymentsFromCourierService {
@@ -15,7 +16,12 @@ export class PaymentsFromCourierService {
 
   async create(createPaymentsFromCourierDto: CreatePaymentsFromCourierDto) {
     try { 
-      
+      const { courier_id, amount, payment_method, payment_date, comment, seller_id } = createPaymentsFromCourierDto;
+      if(payment_method === PaymentMethod.CLICK_TO_MARKET && !seller_id) {
+        throw new BadRequestException("Click_to_market usulida seller_id bo'lishi shart va majburiy !!!")
+      }
+      const payment = this.paymentsFromCourierRepo.create({ courier_id, amount, payment_method, payment_date, comment, seller_id: payment_method === PaymentMethod.CLICK_TO_MARKET ? seller_id: null })
+      return await this.paymentsFromCourierRepo.save(payment)
     } catch (error) {
       return catchError(error.message)
     }
