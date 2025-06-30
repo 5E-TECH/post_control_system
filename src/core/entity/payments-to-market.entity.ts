647ddd5c18c4d25de2682426d552ff61a9d9ec46
Urozov04 <durozov46@gmail.com>
2025-06-30 20:24:17 +0500

    Payment from courier jadvali full bajarildi git add .!

---
+++ src/core/entity/payments-to-market.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity } from "src/common/database/BaseEntity";
+import { Column, Entity } from "typeorm";
+
+@Entity('paymentsToMarket')
+export class PaymentsToMarketEntity extends BaseEntity {
+    @Column({ type: 'int' })
+    market_id: string;
+
+    @Column({ type: 'int' })
+    amount: number;
+
+    @Column({ type: 'varchar' })
+    payment_date: string;
+
+    @Column({type: 'varchar', nullable: true})
+    comment?: string;
+}
