2d86c399851a4bf2008e65a73f674cfab9480adf
Urozov04 <durozov46@gmail.com>
2025-07-03 17:10:43 +0500

    DOing payments to market

--- src/api/payments-to-market/payments-to-market.module.ts
+++ src/api/payments-to-market/payments-to-market.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentsToMarketService } from './payments-to-market.service';
 import { PaymentsToMarketController } from './payments-to-market.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentsToMarketEntity } from 'src/core/entity/payments-to-market.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PaymentsToMarketEntity])],
   controllers: [PaymentsToMarketController],
   providers: [PaymentsToMarketService],
 })
--- src/api/payments-to-market/payments-to-market.service.ts
+++ src/api/payments-to-market/payments-to-market.service.ts
@@ -1,26 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { IsDate } from 'class-validator';
+import { MarketEntity } from './../../core/entity/market.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePaymentsToMarketDto } from './dto/create-payments-to-market.dto';
 import { UpdatePaymentsToMarketDto } from './dto/update-payments-to-market.dto';
 import { PaymentsToMarketEntity } from 'src/core/entity/payments-to-market.entity';
 import { PaymentsToMarketRepository } from 'src/core/repository/paymentstomarket.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { catchError, successRes } from 'src/infrastructure/lib/response';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class PaymentsToMarketService {
   constructor(
     @InjectRepository(PaymentsToMarketEntity)
     private readonly paymentsToMarketRepo: PaymentsToMarketRepository,
+
+    private readonly datasource: DataSource
   ) {  }
 
-  create(createPaymentsToMarketDto: CreatePaymentsToMarketDto) {
-    return 'This action adds a new paymentsToMarket';
+  async create(
+    user: any,
+    createPaymentsToMarketDto: CreatePaymentsToMarketDto) {
+    const transaction = this.datasource.createQueryRunner();
+
+    await transaction.connect();
+
+    await transaction.startTransaction();
+
+    try {
+      const { id } = user;
+      const { market_id, amount, payment_date, comment } = createPaymentsToMarketDto;
+    } catch (error) {
+      return catchError(error.message)
+    }
   }
 
   async findAll() {
-    const paymentToMarket = await 
+    try {
+      const paymentToMarket = await this.paymentsToMarketRepo.find()
+      return successRes(paymentToMarket)
+    } catch (error) {
+      return catchError(error.message)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} paymentsToMarket`;
+  async findOne(id: string) {
+    try {
+      const paymentToMarket = await this.paymentsToMarketRepo.findOne({ where: { id } })
+      if(!paymentToMarket) {
+        throw new NotFoundException('Payments to market not found by id', id)
+      }
+      return paymentToMarket;
+    } catch (error) {
+      return catchError(error.message)
+    }
   }
 }
