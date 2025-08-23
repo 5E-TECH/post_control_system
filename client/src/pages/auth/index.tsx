b5e969012743ecfbc8ccd999e8c3a45953bf73e2
Shodiyor Ergashev <shodiyorergashev970@gmail.com>
2025-08-23 14:38:30 +0500

    client file structure changed

--- client/src/pages/auth/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-
-const Auth = () => {
-    const token = localStorage.getItem('token')
-    return token ? <Outlet /> : <Navigate replace to={'/login'} />
-}
-
-export default React.memo(Auth)
--- client/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export interface IData {
-    phone_number: string
-    password: string
-}
