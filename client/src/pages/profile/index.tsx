77d4997252ed216ea4fa5d0b4663299ab3252bd9
FaxriddinDeveloperr <fazliddinomariipov@gmail.com>
2025-08-29 17:30:11 +0500

    scr

--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { memo } from 'react';
+import ProfilCard1 from '../../shared/components/profil-view-card-1/profil-card-1';
 
-const Profile = () => {
-    return (
-        <div>Profile</div>
-    )
-}
+const Profil = () => {
+  return (
+    <div className="Index">
+      <div>
+        <div className="border w-[343px] h-[724px]"></div>
+        <ProfilCard1 />
+        <div className="border w-[343px] h-[345px]"></div>
+      </div>
+    </div>
+  );
+};
 
-export default React.memo(Profile)
+export default memo(Profil);
---
+++ client/src/shared/components/profil-view-card-1/profil-card-1.tsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+const ProfilCard1 = () => {
+  return (
+    <div className="">
+      <div className="w-[343px] h-[724px]"></div>
+    </div>
+  );
+};
+
+export default memo(ProfilCard1);
