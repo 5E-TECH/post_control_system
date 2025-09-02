afc4bb9fec0f4667fe8ba1b6704567debf1150a7
Urozov04 <durozov46@gmail.com>
2025-09-02 17:03:11 +0500

    Doing select-role page and seperated roles

--- client/src/app/routes.tsx
+++ client/src/app/routes.tsx
@@ -1,5 +1,6 @@
 import { lazy, memo } from "react";
 import { useRoutes } from "react-router-dom";
+import SelectRole from "../pages/select-role";
 
 const Login = lazy(() => import("../pages/login"));
 const Auth = lazy(() => import("../pages/auth"));
@@ -20,6 +21,10 @@ const Profile = lazy(() => import("../pages/profile"));
 
 const AppRouters = () => {
   return useRoutes([
+    {
+      path: "/select-role",
+      element: <SelectRole/>
+    },
     {
       path: "/login",
       element: <Login />,
