91ab7ef551234bbc9d203e77ae4bcd01c1f56802
Shodiyor Ergashev <shodiyorergashev970@gmail.com>
2025-09-04 12:55:26 +0500

    payment from courier modified

--- server/src/api/market/market.module.ts
+++ server/src/api/market/market.module.ts
@@ -7,9 +7,12 @@ import { BcryptEncryption } from 'src/infrastructure/lib/bcrypt';
 import { Token } from 'src/infrastructure/lib/token-generator/token';
 import { CashEntity } from 'src/core/entity/cash-box.entity';
 import { CashBoxService } from '../cash-box/cash-box.service';
+import { CashboxHistoryEntity } from 'src/core/entity/cashbox-history.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MarketEntity, CashEntity])],
+  imports: [
+    TypeOrmModule.forFeature([MarketEntity, CashEntity, CashboxHistoryEntity]),
+  ],
   controllers: [MarketController],
   providers: [MarketService, BcryptEncryption, Token, CashBoxService],
 })
