415270d21eb00c4076c43e3274165aca9abcad9a
Zuxriddinoff <zuxriddinovoff@gmail.com>
2025-09-08 12:39:56 +0500

    order page navigate

--- client/src/pages/orders/components/customer detail/index.tsx
+++ client/src/pages/orders/components/customer detail/index.tsx
@@ -27,9 +27,9 @@ const CustomerDetail = () => {
         </div>
       </div>
       <div className="mx-5 mb-6 flex gap-3 flex-col text-[15px] text-[#E7E3FCB2] dark:text-[#E7E3FCE5]">
-        <Info text={"Contact info"} className={"text-[15px] dark:text-[#E7E3FCE5]"}/>
+        <Info text={"Contact info"} path="/orders/customer/detail" className={"text-[15px] dark:text-[#E7E3FCE5]"}/>
         <div>
-            <h2>Phone Number: +998913607434</h2>
+            <h2 className="text-[#2E263DB2] dark:text-[#E7E3FCB2]">Phone Number: +998913607434</h2>
         </div>
       </div>
     </div>
