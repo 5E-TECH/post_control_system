1cd578d5ca4c320fc637dd6345e0169b7a33a316
DiyorMarufov <marufovdiyor9@gmail.com>
2025-09-08 12:23:02 +0500

    success changed

--- client/src/pages/orders/components/button/success.tsx
+++ client/src/pages/orders/components/button/success.tsx
@@ -1,13 +1,12 @@
 import { Button } from "antd";
-import { ArrowRight } from "lucide-react";
 import { memo, type FC, type JSX } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface Props {
   text: string;
+  path:string
   icon?: JSX.Element;
   className?: string;
-  path:string
 }
 
 const Success: FC<Props> = ({ text, icon, path,className }) => {
@@ -20,9 +19,10 @@ const Success: FC<Props> = ({ text, icon, path,className }) => {
       onClick={onClick}
       className={`w-[91px]! h-[38px]! bg-[var(--color-bg-sy)]! text-[#ffffff]! hover:opacity-85! hover:outline-none! dark:border-none! ${className}`}
     >
-      {text} {icon ? icon : <ArrowRight className="h-[13px] w-[13px]" />}
+      {text} {icon}
     </Button>
   );
-};
+}
+
 
 export default memo(Success);
