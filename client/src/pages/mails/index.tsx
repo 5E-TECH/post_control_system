5d9e0547bfa2f38d60a1df9ab7e69b2f31cc5472
DiyorMarufov <marufovdiyor9@gmail.com>
2025-09-09 07:41:48 +0500

    mails page added

--- client/src/pages/mails/index.tsx
+++ client/src/pages/mails/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Mails = () => {
-    return (
-        <div>Mails</div>
-    )
-}
+  return (
+    <div className="h-[800px] p-5">
+      <div className="grid grid-cols-3 gap-18 h-[60px]">
+        <NavLink to={""} className={"flex items-center justify-center shadow-lg rounded-md bg-[#ffffff] text-[20px]"}>
+          Bugungi pochtalar
+        </NavLink>
+        <NavLink to={""} className={"flex items-center justify-center shadow-lg rounded-md bg-[#ffffff] text-[20px]"}>
+          Qaytgan pochtalar
+        </NavLink>
+        <NavLink to={""} className={"flex items-center justify-center shadow-lg rounded-md bg-[#ffffff] text-[20px]"}>
+          Eski pochtalar
+        </NavLink>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default React.memo(Mails)
+export default React.memo(Mails);
