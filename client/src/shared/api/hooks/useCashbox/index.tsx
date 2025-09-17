a887d15001f6c020290bf1ffa7e8832a87300235
Zuxriddinoff <zuxriddinovoff@gmail.com>
2025-09-17 16:42:38 +0500

    cash box logic for courier

--- client/src/shared/api/hooks/useCashbox/index.tsx
+++ client/src/shared/api/hooks/useCashbox/index.tsx
@@ -1,9 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { api } from "../..";
 
 export const cashbox = "cashbox";
 
 export const useCashBox = () => {
+  const client = useQueryClient();
+
+
+  const createPaymentCourier = useMutation({
+    mutationFn: (data: any) => api.post("cashbox/payment/courier", data),
+    onSuccess: () => client.invalidateQueries({ queryKey: [cashbox] }),
+  });
+
   const getCashBoxById = (id: string) =>
     useQuery({
       queryKey: [cashbox],
@@ -14,8 +22,7 @@ export const useCashBox = () => {
     useQuery({
       queryKey: [cashbox],
       queryFn: () => api.get(`cashbox/all-info`).then((res) => res.data),
-      
     });
 
-  return { getCashBoxById, getCashBoxInfo };
+  return { getCashBoxById, getCashBoxInfo, createPaymentCourier };
 };
