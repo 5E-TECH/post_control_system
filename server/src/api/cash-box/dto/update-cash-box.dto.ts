ff125caf242598c97efbe4f1d2a125dc4fc7cc40
Shodiyor Ergashev <shodiyorergashev970@gmail.com>
2025-09-17 14:09:05 +0500

    canceled and cancel receive posts updated

--- server/src/api/cash-box/dto/update-cash-box.dto.ts
+++ server/src/api/cash-box/dto/update-cash-box.dto.ts
@@ -1,22 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Operation_type, Source_type } from 'src/common/enums';
+import {
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+import { Manual_payment_methods } from 'src/common/enums';
 
 export class UpdateCashBoxDto {
   @ApiProperty({
     type: Number,
     example: 1_000_000,
   })
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
   balance: number;
 
   @ApiProperty({
-    enum: Source_type,
-    example: Source_type.BILLS,
+    enum: Manual_payment_methods,
+    example: Manual_payment_methods.CASH,
   })
-  type: Source_type;
+  @IsNotEmpty()
+  @IsIn(Object.values(Manual_payment_methods))
+  type: Manual_payment_methods;
 
   @ApiProperty({
     type: String,
     example: 'Xodimlar kechki ovqati uchun',
   })
+  @IsOptional()
+  @IsString()
   comment: string;
 }
