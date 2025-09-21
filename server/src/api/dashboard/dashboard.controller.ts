41ba1868de08d86bad76e12eedc2949a479311e0
Shodiyor Ergashev <shodiyorergashev970@gmail.com>
2025-09-21 20:43:39 +0500

    dashboard for courier and for market

--- server/src/api/dashboard/dashboard.controller.ts
+++ server/src/api/dashboard/dashboard.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { DashboardService } from './dashboard.service';
+import { AcceptRoles } from 'src/common/decorator/roles.decorator';
+import { Roles } from 'src/common/enums';
+import { JwtGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @ApiTags('Dashboard')
 @Controller('dashboard')
@@ -8,12 +12,61 @@ export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Get('overview')
-  @ApiQuery({ name: 'startDate', required: false, description: 'Start date (ISO string)' })
-  @ApiQuery({ name: 'endDate', required: false, description: 'End date (ISO string)' })
+  @ApiQuery({
+    name: 'startDate',
+    required: false,
+    description: 'Start date (ISO string)',
+  })
+  @ApiQuery({
+    name: 'endDate',
+    required: false,
+    description: 'End date (ISO string)',
+  })
+  @UseGuards(JwtGuard, RolesGuard)
+  @AcceptRoles(Roles.ADMIN, Roles.SUPERADMIN)
   getOverview(
     @Query('startDate') startDate?: string,
     @Query('endDate') endDate?: string,
   ) {
     return this.dashboardService.getOverview({ startDate, endDate });
+  }
+
+  @Get('overview/courier')
+  @ApiQuery({
+    name: 'startDate',
+    required: false,
+    description: 'Start date (ISO string)',
+  })
+  @ApiQuery({
+    name: 'endDate',
+    required: false,
+    description: 'End date (ISO string)',
+  })
+  @AcceptRoles(Roles.COURIER)
+  getStatsForCourier(
+    @Query('startDate') startDate?: string,
+    @Query('endDate') endDate?: string,
+  ) {
+    return this.dashboardService.getStatsForCourier({ startDate, endDate });
+  }
+
+  @Get('overview/market')
+  @ApiQuery({
+    name: 'startDate',
+    required: false,
+    description: 'Start date (ISO string)',
+  })
+  @ApiQuery({
+    name: 'endDate',
+    required: false,
+    description: 'End date (ISO string)',
+  })
+  @UseGuards(JwtGuard, RolesGuard)
+  @AcceptRoles(Roles.MARKET)
+  getStatsForMarket(
+    @Query('startDate') startDate?: string,
+    @Query('endDate') endDate?: string,
+  ) {
+    return this.dashboardService.getStatsForMarket({ startDate, endDate });
   }
 }
