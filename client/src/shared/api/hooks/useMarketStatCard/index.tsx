4a48693190554632b1b3de4e9e96f33571674aa0
Urozov04 <durozov46gmail.com>
2025-09-22 14:40:43 +0500

    Added new hooks

--- client/src/shared/api/hooks/useMarketStatCard/index.tsx
+++ client/src/shared/api/hooks/useMarketStatCard/index.tsx
@@ -14,7 +14,7 @@ export const useChart = () => {
       queryKey: [chart, params],
       queryFn: () =>
         api
-          .get("dashboard/overview", { params: { ...params } })
+          .get("dashboard/overview/market", { params: { ...params } })
           .then((res) => res.data),
     });
   };
