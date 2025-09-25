551d1a728a175533fd051eb8aa420ec9c662683c
abdulfattoh-dev <abdulfattoh.dev@gmail.com>
2025-09-25 16:00:47 +0500

    full translated user pages

--- client/src/pages/users/pages/create-market/index.tsx
+++ client/src/pages/users/pages/create-market/index.tsx
@@ -3,6 +3,7 @@ import { Button, Form, Input, type FormProps } from "antd";
 import { ArrowRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../../../shared/api/hooks/useRegister";
+import { useTranslation } from "react-i18next";
 
 type FieldType = {
   name: string;
@@ -13,6 +14,7 @@ type FieldType = {
 };
 
 const CreateMarket = () => {
+  const { t } = useTranslation("users");
   const { createUser } = useUser("market");
   const navigate = useNavigate();
 
@@ -32,47 +34,46 @@ const CreateMarket = () => {
   return (
     <div className="w-[420px]">
       <h1 className="font-medium text-[24px] text-[#2E263DE5] dark:text-[#E7E3FCE5]">
-        Market yaratish
+        {t("marketTitle")}
       </h1>
       <span className="font-normal text-[15px] text-[#2E263DB2] dark:text-[#E7E3FCB2]">
-        Market ma'lumotlarini kiriting
+        {t("marketDescription")}
       </span>
       <Form onFinish={onFinish} className="pt-5!">
         <Form.Item
           name="name"
-          rules={[{ required: true, message: "Market nomini kiriting" }]}
+          rules={[{ required: true, message: t("enterName") }]}
         >
           <Input
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Market nomini kiriting"
+            placeholder={t("enterName")}
           />
         </Form.Item>
 
         <Form.Item
           name="phone_number"
           rules={[
-            { required: true, message: "Telefon raqamni kiriting" },
+            { required: true, message: t("enterPhoneNumber") },
             {
               pattern: /^\+998\d{9}$/,
-              message:
-                "Telefon raqam +998 bilan boshlanishi va 9 raqamdan iborat bo‘lishi kerak",
+              message: t("phoneNumberPattern"),
             },
           ]}
         >
           <Input
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Telefon raqamni kiriting"
+            placeholder={t("enterPhoneNumber")}
           />
         </Form.Item>
 
         <Form.Item
           name="tariff_home"
           rules={[
-            { required: true, message: "Uy tarifi qiymatini kiriting" },
+            { required: true, message: t("enterHomeTariff") },
             {
               type: "number",
               min: 0,
-              message: "Tarif 0 dan kam bo‘lmasligi kerak",
+              message: t("tariffMin"),
               transform: (value) => Number(value),
             },
           ]}
@@ -80,18 +81,18 @@ const CreateMarket = () => {
           <Input
             type="number"
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Uy tarifini kiriting (so'm)"
+            placeholder={t("enterHomeTariff")}
           />
         </Form.Item>
 
         <Form.Item
           name="tariff_center"
           rules={[
-            { required: true, message: "Markaz tarifi qiymatini kiriting" },
+            { required: true, message: t("enterCenterTariff") },
             {
               type: "number",
               min: 0,
-              message: "Tarif 0 dan kam bo‘lmasligi kerak",
+              message: t("tariffMin"),
               transform: (value) => Number(value),
             },
           ]}
@@ -99,18 +100,18 @@ const CreateMarket = () => {
           <Input
             type="number"
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Markaz tarifini kiriting (so'm)"
+            placeholder={t("enterCenterTariff")}
           />
         </Form.Item>
 
         <Form.Item
           name="password"
-          rules={[{ required: true, message: "Parolni kiriting" }]}
+          rules={[{ required: true, message: t("enterPassword") }]}
         >
           <Input
             type="password"
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Parolni kiriting"
+            placeholder={t("enterPassword")}
           />
         </Form.Item>
 
@@ -122,7 +123,7 @@ const CreateMarket = () => {
             htmlType="submit"
             className="bg-[#8C57FF]! w-[115px]"
           >
-            <span>Yaratish</span>
+            <span>{t("create")}</span>
             <ArrowRight className="w-[12px] h-[12px]" />
           </Button>
         </div>
--- client/src/pages/users/pages/create-registrator/index.tsx
+++ client/src/pages/users/pages/create-registrator/index.tsx
@@ -3,6 +3,7 @@ import { Button, Form, Input, type FormProps } from "antd";
 import { ArrowRight } from "lucide-react";
 import { useUser } from "../../../../shared/api/hooks/useRegister";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 type FieldType = {
   first_name: string;
@@ -14,6 +15,7 @@ type FieldType = {
 };
 
 const CreateRegistrator = () => {
+  const { t } = useTranslation("users");
   const { createUser } = useUser("registrator");
   const navigate = useNavigate();
 
@@ -33,57 +35,56 @@ const CreateRegistrator = () => {
   return (
     <div className="w-[420px]">
       <h1 className="font-medium text-[24px] text-[#2E263DE5] dark:text-[#E7E3FCE5]">
-        Ro'yxatchini yaratish
+        {t("registratorTitle")}
       </h1>
       <span className="font-normal text-[15px] text-[#2E263DB2] dark:text-[#E7E3FCB2]">
-        Ro'yxatchini ma'lumotlarini kiriting
+        {t("registratorDescription")}
       </span>
       <Form onFinish={onFinish} className="pt-5!">
         <Form.Item
           name="name"
-          rules={[{ required: true, message: "Ismni kiriting" }]}
+          rules={[{ required: true, message: t("enterName") }]}
         >
           <Input
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Ismni kiriting"
+            placeholder={t("enterName")}
           />
         </Form.Item>
         <Form.Item
           name="phone_number"
           rules={[
-            { required: true, message: "Telefon raqamni kiriting" },
+            { required: true, message: t("enterPhoneNumber") },
             {
               pattern: /^\+998\d{9}$/,
-              message:
-                "Telefon raqam +998 bilan boshlanishi va 9 raqamdan iborat bo‘lishi kerak",
+              message: t("phoneNumberPattern"),
             },
           ]}
         >
           <Input
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Telefon raqamni kiriting"
+            placeholder={t("enterPhoneNumber")}
           />
         </Form.Item>
 
         <Form.Item
           name="password"
-          rules={[{ required: true, message: "Parolni kiriting" }]}
+          rules={[{ required: true, message: t("enterPassword") }]}
         >
           <Input
             type="password"
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Parolni kiriting"
+            placeholder={t("enterPassword")}
           />
         </Form.Item>
 
         <Form.Item
           name="salary"
           rules={[
-            { required: true, message: "Oylikni kiriting" },
+            { required: true, message: t("enterSalary") },
             {
               type: "number",
               min: 0,
-              message: "Oylik 0 dan kam bo‘lmasligi kerak",
+              message: t("salaryMin"),
               transform: (value) => Number(value),
             },
           ]}
@@ -91,7 +92,7 @@ const CreateRegistrator = () => {
           <Input
             type="number"
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="Oylikni kiriting (so'm)"
+            placeholder={t("enterSalary")}
           />
         </Form.Item>
 
@@ -102,7 +103,7 @@ const CreateRegistrator = () => {
               type: "number",
               min: 1,
               max: 30,
-              message: "To‘lov kuni 1 dan 30 gacha bo‘lishi kerak",
+              message: t("paymentDayRange"),
               transform: (value) => Number(value),
             },
           ]}
@@ -110,7 +111,7 @@ const CreateRegistrator = () => {
           <Input
             type="number"
             className="h-[48px] dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#E7E3FC66]! dark:text-[#E7E3FC66]!"
-            placeholder="To'lov kunini kiriting (1-30)"
+            placeholder={t("enterPaymentDay")}
           />
         </Form.Item>
 
@@ -122,7 +123,7 @@ const CreateRegistrator = () => {
             htmlType="submit"
             className="bg-[#8C57FF]! w-[115px]"
           >
-            <span>Yaratish</span>
+            <span>{t("create")}</span>
             <ArrowRight className="w-[12px] h-[12px]" />
           </Button>
         </div>
