56b20910ddd35565fe79376613c36edbc419c27e
DiyorMarufov <marufovdiyor9@gmail.com>
2025-09-30 11:55:32 +0500

    courier-orders changed and user findone relations added

--- client/src/pages/orders/components/courier/waiting-orders/index.tsx
+++ client/src/pages/orders/components/courier/waiting-orders/index.tsx
@@ -1,13 +1,31 @@
-import { memo } from "react";
+import { memo, useRef, useState, type MouseEvent } from "react";
 import { useOrder } from "../../../../../shared/api/hooks/useOrder";
 import EmptyPage from "../../../../../shared/components/empty-page";
-import { Button, Pagination, type PaginationProps } from "antd";
-import { useApiNotification } from "../../../../../shared/hooks/useApiNotification";
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Pagination,
+  type FormProps,
+  type PaginationProps,
+} from "antd";
+// import { useApiNotification } from "../../../../../shared/hooks/useApiNotification";
 import { useNavigate } from "react-router-dom";
 import { useParamsHook } from "../../../../../shared/hooks/useParams";
+import Popup from "../../../../../shared/ui/Popup";
+import { X } from "lucide-react";
+import { useApiNotification } from "../../../../../shared/hooks/useApiNotification";
+
+type FieldType = {
+  comment?: string;
+  extraCost?: number;
+};
 
 const WaitingOrders = () => {
   const navigate = useNavigate();
+  const orderId = useRef<string | null>(null);
+  const urlType = useRef<string | null>(null);
 
   const { getParam, setParam, removeParam } = useParamsHook();
   const page = Number(getParam("page") || 1);
@@ -16,31 +34,56 @@ const WaitingOrders = () => {
   const { data } = getCourierOrders({ status: "waiting" });
   const total = data?.data?.total || 0;
 
+  const [form] = Form.useForm<FieldType>();
   const { handleSuccess, handleApiError } = useApiNotification();
-  const handleSellOrder = (id: string) => {
-    sellOrder.mutate(
-      { id, data: {} },
-      {
-        onSuccess: () => {
-          handleSuccess("Buyurtma muvaffaqiyatli sotildi");
-        },
-        onError: (err: any) =>
-          handleApiError(err, "Buyurtmani sotishda xatolik yuz berdi"),
-      }
-    );
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    const id = orderId.current;
+    const type = urlType.current;
+
+    if (type === "sell") {
+      sellOrder.mutate(
+        { id: id as string, data: values },
+        {
+          onSuccess: () => {
+            setIsShow(false);
+            handleSuccess("Buyurtma muvaffaqiyatli sotildi");
+          },
+          onError: (err: any) =>
+            handleApiError(err, "Buyurtmani sotishda xatolik yuz berdi"),
+        }
+      );
+    } else {
+      cancelOrder.mutate(
+        { id: id as string, data: values },
+        {
+          onSuccess: () => {
+            setIsShow(false);
+            handleSuccess("Buyurtma muvaffaqiyatli bekor qilindi");
+          },
+          onError: (err: any) =>
+            handleApiError(err, "Buyurtmani bekor qilishda xatolik yuz berdi"),
+        }
+      );
+    }
+  };
+  const handleSellOrder = (
+    e: MouseEvent<HTMLButtonElement | HTMLElement>,
+    id: string
+  ) => {
+    e.stopPropagation();
+    orderId.current = id;
+    urlType.current = "sell";
+    setIsShow(true);
   };
 
-  const handleCancelOrder = (id: string) => {
-    cancelOrder.mutate(
-      { id, data: {} },
-      {
-        onSuccess: () => {
-          handleSuccess("Buyurtma muvaffaqiyatli bekor qilindi");
-        },
-        onError: (err: any) =>
-          handleApiError(err, "Buyurtmani bekor qilishda xatolik yuz berdi"),
-      }
-    );
+  const handleCancelOrder = (
+    e: MouseEvent<HTMLButtonElement | HTMLElement>,
+    id: string
+  ) => {
+    e.stopPropagation();
+    orderId.current = id;
+    urlType.current = "cancel";
+    setIsShow(true);
   };
 
   const onChange: PaginationProps["onChange"] = (newPage, limit) => {
@@ -57,6 +100,8 @@ const WaitingOrders = () => {
     }
   };
 
+  const [isShow, setIsShow] = useState(false);
+
   return data?.data?.data?.length > 0 ? (
     <div>
       <table className="w-full">
@@ -70,49 +115,49 @@ const WaitingOrders = () => {
             <th>
               <div className="flex items-center gap-10">
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
-                <span>CUSTOMER</span>
+                <span>MIJOZ</span>
               </div>
             </th>
             <th>
               <div className="flex items-center gap-10">
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
-                <span>PHONE</span>
+                <span>TEL RAQAMI</span>
               </div>
             </th>
             <th>
               <div className="flex items-center gap-10">
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
-                <span>ADDRESS</span>
+                <span>MAZNIL</span>
               </div>
             </th>
             <th>
               <div className="flex items-center gap-10">
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
-                <span>MARKET</span>
+                <span>FIRMA</span>
               </div>
             </th>
             <th>
               <div className="flex items-center gap-10">
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
-                <span>STATUS</span>
+                <span>HOLATI</span>
               </div>
             </th>
             <th>
               <div className="flex items-center gap-10">
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
-                <span>PRICE</span>
+                <span>NARXI</span>
               </div>
             </th>
             <th>
               <div className="flex items-center gap-10">
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
-                <span>STOCK</span>
+                <span>OMBOR</span>
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
               </div>
             </th>
             <th>
               <div className="flex items-center justify-center gap-30">
-                <span>ACTIONS</span>
+                <span>HARAKAT</span>
                 <div className="w-[2px] h-[14px] bg-[#2E263D1F] dark:bg-[#524B6C]"></div>
               </div>
             </th>
@@ -125,8 +170,13 @@ const WaitingOrders = () => {
               key={item?.id}
               className="h-[56px] hover:bg-[#f6f7fb] dark:hover:bg-[#3d3759] cursor-pointer"
             >
-              <td className="pl-10">{inx + 1}</td>
-              <td className="pl-10 text-[#2E263DE5] text-[15px] dark:text-[#d5d1eb]">
+              <td className="data-cell pl-10" data-cell="#">
+                {inx + 1}
+              </td>
+              <td
+                className="pl-10 text-[#2E263DE5] text-[15px] dark:text-[#d5d1eb]"
+                data-cell="MIJOZ ISMI"
+              >
                 {item?.customer?.name}
               </td>
               <td className="pl-10 text-[#2E263DB2] text-[15px] dark:text-[#d5d1eb]">
@@ -154,7 +204,7 @@ const WaitingOrders = () => {
                   <Button
                     disabled={sellOrder.isPending}
                     loading={sellOrder.isPending}
-                    onClick={() => handleSellOrder(item?.id)}
+                    onClick={(e) => handleSellOrder(e, item?.id)}
                     className="bg-[var(--color-bg-sy)]! text-[#ffffff]! border-none! hover:opacity-80"
                   >
                     Sotish
@@ -162,7 +212,7 @@ const WaitingOrders = () => {
                   <Button
                     disabled={cancelOrder.isPending}
                     loading={cancelOrder.isPending}
-                    onClick={() => handleCancelOrder(item?.id)}
+                    onClick={(e) => handleCancelOrder(e, item?.id)}
                     className="bg-red-500! text-[#ffffff]! border-none! hover:opacity-80"
                   >
                     Bekor qilish
@@ -174,7 +224,7 @@ const WaitingOrders = () => {
         </tbody>
       </table>
 
-      <div className="flex justify-center">
+      <div className="flex justify-center pt-5">
         <Pagination
           showSizeChanger
           current={page}
@@ -183,6 +233,57 @@ const WaitingOrders = () => {
           onChange={onChange}
         />
       </div>
+      <Popup isShow={isShow} onClose={() => setIsShow(false)}>
+        <div className="w-[400px] bg-[#ffffff] shadow-lg rounded-md relative pb-4">
+          <X
+            className="absolute top-2.5 right-2.5 cursor-pointer hover:bg-gray-200"
+            onClick={() => setIsShow(false)}
+          />
+
+          <Form
+            initialValues={{}}
+            form={form}
+            onFinish={onFinish}
+            className="px-8!"
+          >
+            <div className="pt-10">
+              <span className="">Izoh</span>
+              <Form.Item name="comment">
+                <Input.TextArea
+                  className="py-4! dark:bg-[#312D4B]! dark:border-[#E7E3FC38]! dark:placeholder:text-[#A9A5C0]! dark:text-[#E7E3FC66]!"
+                  placeholder="Izoh qoldiring (ixtiyoriy)"
+                  style={{ resize: "none" }}
+                />
+              </Form.Item>
+            </div>
+
+            <div>
+              <span>Qo'shimcha (pul)</span>
+              <Form.Item name="extraCost">
+                <InputNumber
+                  placeholder="Qo'shimcha pul"
+                  className="h-[40px]! w-full!"
+                  formatter={(value) =>
+                    value
+                      ? value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                      : ""
+                  }
+                  parser={(value) => value?.replace(/,/g, "") || ""}
+                />
+              </Form.Item>
+            </div>
+
+            <div className="flex justify-center">
+              <Button
+                htmlType="submit"
+                className="px-5! py-4! bg-[var(--color-bg-sy)]! text-[#ffffff]!"
+              >
+                Tasdiqlash
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </Popup>
     </div>
   ) : (
     <div className="h-[65vh]">
