dc1004c195ef4f498f148304c0d08735a98c5001
Zuxriddinoff <zuxriddinovoff@gmail.com>
2025-10-02 17:45:40 +0500

    nimadir

--- client/src/pages/regions/index.tsx
+++ client/src/pages/regions/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import UzbekistanMap from './components/map'
+import { useNavigate } from 'react-router-dom'
 
 const Regions = () => {
+    const navigate = useNavigate()
     return (
         <div>Regions
             <UzbekistanMap/>
+            <button onClick={() => navigate("region-districts")}>
+                Districni tartiblash
+            </button>
         </div>
     )
 }
